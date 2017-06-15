e6981743bc71ebab11c3f886b9e68e78da661e66
david850519 <david850519@gmail.com>
2017-06-15 14:10:28 +0800

    Add files via upload

--- web_final2/js/all.js
+++ web_final2/js/all.js
@@ -469,13 +469,14 @@ function PutCart(){
   			var Buy_Num=document.getElementById('Buy_Num').value;
   			if(Buy_Num==""){
   				alert('請輸入數字');
+  				return;
   			}else{
   				alert(Buy_Num);
   			}
 		}else{
 			alert("請先登入");
+			return;
 		}
 	});
-	break;
 }
 
