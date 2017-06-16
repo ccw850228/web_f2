e895d25e45452ab1696c20ea8f8ea924cb919d01
ccw850228 <ccw850228@gmail.com>
2017-06-16 21:42:31 +0800

    Add files via upload

--- web_final2/js/all.js
+++ web_final2/js/all.js
@@ -721,9 +721,8 @@ function ShowBuyRecord(){
 				var br = document.createElement("br");
 				for(i=0;i<num;i++){
 					console.log(Record_Content[i]);
-				
 				var td2_text =document.createTextNode(Record_Content[i]);
-
+				td2_text.appendChild(br);
 				td2.appendChild(td2_text);
 				
 				}
